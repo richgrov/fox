55d92086cf0f4db72921aa3fb57d88aa4661bd84
Richard Grover <38992201+richgrov@users.noreply.github.com>
2024-10-29 10:26:45 -0600

    Move preprocessor structs into header

--- src/preprocessor/preproc.c
+++ src/preprocessor/preproc.c
@@ -9,32 +9,6 @@
 #include "config.h"
 #include "operator.h"
 
-typedef enum {
-   ERR_EOF,
-   ERR_UNEXPECTED,
-} PreprocError;
-
-typedef enum {
-   PROC_ERROR, // not in the standard, but used internally
-   PROC_IDENTIFIER,
-   PROC_NUMBER,
-   PROC_CHAR,
-   PROC_STR,
-   PROC_CHEVRON_STR, // #include strings quoted in < >
-   PROC_OPERATOR,
-   PROC_MISC, // non-whitespace, non-comment character that isn't any of the above
-} PreprocType;
-
-typedef struct {
-   PreprocType type;
-   union {
-      char char_data;
-      char *str_data;
-      Operator op_data;
-      PreprocError err_data;
-   };
-} PreprocToken;
-
 static void token_free(PreprocToken *tok) {
    switch (tok->type) {
    case PROC_IDENTIFIER:
