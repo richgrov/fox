13655e584d57894036239cc390aa4ef13c2e169b
Richard Grover <38992201+richgrov@users.noreply.github.com>
2024-10-30 19:43:44 -0600

    Initial dictionary implementation

--- src/preprocessor/macro_dict.c
+++ src/preprocessor/macro_dict.c
@@ -1,6 +1,9 @@
 #include "macro_dict.h"
 
+#include <stddef.h>
 #include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
 
 #if UINTPTR_MAX == 0xFFFFFFFF
 
@@ -23,4 +26,42 @@ static unsigned long fnv1a_hash(const char *txt) {
    }
 
    return result;
+}
+
+void macro_dict_put(MacroDictionary *dict, const char *key, const char *value) {
+   unsigned long hash = fnv1a_hash(key);
+
+   MacroDictionaryNode *node = &dict->buckets[hash % 32];
+   if (node->key == NULL) {
+      node->key = key;
+      node->value = value;
+      node->hash = hash;
+      return;
+   }
+
+   while (node->next != NULL) {
+      node = node->next;
+   }
+
+   MacroDictionaryNode *new = malloc(sizeof(*node->next));
+   memset(new, 0, sizeof(*new));
+   new->key = key;
+   new->value = value;
+   new->hash = hash;
+   node->next = new;
+}
+
+const char *macro_dict_get(MacroDictionary *dict, const char *key) {
+   unsigned long hash = fnv1a_hash(key);
+
+   MacroDictionaryNode *node = &dict->buckets[hash % 32];
+   while (node != NULL) {
+      if (hash == node->hash && strcmp(key, node->key) == 0) {
+         return node->value;
+      }
+
+      node = node->next;
+   }
+
+   return NULL;
 }
--- src/preprocessor/macro_dict.h
+++ src/preprocessor/macro_dict.h
@@ -1,7 +1,19 @@
 #ifndef FOX_MACRO_DICT_H_
 #define FOX_MACRO_DICT_H_
 
+typedef struct MacroDictionaryNode {
+   struct MacroDictionaryNode *next;
+   const char *key;
+   const char *value;
+   unsigned long hash;
+} MacroDictionaryNode;
+
 typedef struct {
+   struct MacroDictionaryNode buckets[32];
 } MacroDictionary;
+
+void macro_dict_put(MacroDictionary *dict, const char *key, const char *value);
+
+const char *macro_dict_get(MacroDictionary *dict, const char *key);
 
 #endif // !FOX_MACRO_DICT_H_
